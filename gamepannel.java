42f90b1509510995a1fee5a07a6998c585cb052f
sgobi <g2k2@live.com>
2024-10-17 23:18:33 +0530

    we did title with tiles

--- gamepannel.java
+++ gamepannel.java
@@ -2,6 +2,8 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -11,21 +13,44 @@ import javax.swing.JPanel;
 
 public class gamepannel implements ActionListener {
 
-JPanel titlePanel =new JPanel();
-JPanel buttonPannel =new JPanel();
-JFrame gameFrame =new JFrame();
-JLabel textLabel =new JLabel();
-JButton[] buttons = new JButton[9];
+    JPanel titlePanel = new JPanel();
+    JPanel buttonPannel = new JPanel();
+    JFrame gameFrame = new JFrame();
+    JLabel textLabel = new JLabel();
+    JButton[] buttons = new JButton[9];
 
+    gamepannel() {
+        gameFrame.setTitle("TicTacToa");
+        gameFrame.setSize(new Dimension(800, 800));
+        gameFrame.getContentPane().setBackground(new Color(50, 50, 50, 50));
+        gameFrame.setLayout(new BorderLayout());
+        gameFrame.setLocationRelativeTo(null);
+        gameFrame.setVisible(true);
 
-    gamepannel()
-    {
-gameFrame.setTitle("TicTacToa");
-gameFrame.setSize(new Dimension(800,800));
-gameFrame.getContentPane().setBackground(new Color(50,50,50,50));
-gameFrame.setLayout(new BorderLayout());
-gameFrame.setLocationRelativeTo(null);
-gameFrame.setVisible(true);
+        textLabel.setBackground(new Color(25, 25, 25));
+        textLabel.setForeground(new Color(25, 225,0 ));
+        textLabel.setFont(new Font("Ink Free", Font.BOLD, 75));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic Tac Toa");
+        textLabel.setOpaque(true);
+
+        titlePanel.setLayout(new BorderLayout());
+        titlePanel.setBounds(0, 0, 800, 100);
+      
+        buttonPannel.setLayout(new GridLayout(3,3));
+        buttonPannel.setBackground(new Color(150, 150, 150));
+
+        for (int i= 0; i<9;i++){
+            buttons[i] = new JButton();
+            buttons[i].setFont(new Font("MV Boli", Font.BOLD, 120));
+            buttonPannel.add(buttons[i]);
+            buttonPannel.setFocusable(false);
+            buttons[i].addActionListener(this);
+        }
+
+        titlePanel.add(textLabel);
+        gameFrame.add(titlePanel,BorderLayout.NORTH);
+        gameFrame.add(buttonPannel);
 
 
     }
