621c478a6bd4de5ec9cb344a2c4c42e4dab2294e
sgobi <g2k2@live.com>
2024-10-18 10:33:01 +0530

    now randomly game will choose a player (O or X)

--- gamepannel.java
+++ gamepannel.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -18,7 +19,8 @@ public class gamepannel implements ActionListener {
     JFrame gameFrame = new JFrame();
     JLabel textLabel = new JLabel();
     JButton[] buttons = new JButton[9];
-
+    Random random = new Random();
+    boolean playerOneTurn =false;
     gamepannel() {
         gameFrame.setTitle("TicTacToa");
         gameFrame.setSize(new Dimension(800, 800));
@@ -39,7 +41,7 @@ public class gamepannel implements ActionListener {
       
         buttonPannel.setLayout(new GridLayout(3,3));
         buttonPannel.setBackground(new Color(150, 150, 150));
-
+     
         for (int i= 0; i<9;i++){
             buttons[i] = new JButton();
             buttons[i].setFont(new Font("MV Boli", Font.BOLD, 120));
@@ -52,8 +54,34 @@ public class gamepannel implements ActionListener {
         gameFrame.add(titlePanel,BorderLayout.NORTH);
         gameFrame.add(buttonPannel);
 
-
+        gameStart();
     }
+
+
+    public void gameStart(){
+        firstTurn();
+    }
+public void firstTurn(){
+        try {
+            //  int palyer =random.nextInt(2); //here 0 or 1 that mean O or X
+            Thread.sleep(2000);
+        } catch (InterruptedException ex) {
+        }
+    if (random.nextInt(2)==0) {
+        playerOneTurn=true;
+        textLabel.setText("X Turn");
+    }
+    else{
+        playerOneTurn=false;
+        textLabel.setText("O Turn");
+    }
+}
+public void check(){}
+
+public void xPlayerWins(){}
+public void oPlayerWins(){}
+
+
 
     @Override
     public void actionPerformed(ActionEvent ae) {
